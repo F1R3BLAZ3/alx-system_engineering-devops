83d95e947e71e4cc372dd8633026e089573d202a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-23 19:47:22 +0000

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,36 +18,37 @@ def get_employee_todo_progress(employee_id):
     Returns:
         None
     """
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+    base_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{base_url}/users/{employee_id}'
+    todo_url = f'{user_url}/todos'
 
     # Fetch user data
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get("name")
+    user_data = requests.get(user_url).json()
+    employee_name = user_data.get('name')
 
     # Fetch TODO list for the user
-    todo_response = requests.get(todo_url)
-    todo_list = todo_response.json()
+    todo_list = requests.get(todo_url).json()
 
     # Calculate progress
     total_tasks = len(todo_list)
-    done_tasks = sum(1 for task in todo_list if task.get("completed"))
+    done_tasks = sum(1 for task in todo_list if not task.get('completed'))
 
     # Display progress
     print(f"Employee {employee_name} is done with tasks "
           f"({done_tasks}/{total_tasks}):")
-
     for task in todo_list:
-        if task.get("completed"):
-            print(f"\t {task.get('title')}")
+        if not task.get('completed'):
+            print(f'\t {task.get("title")}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print('Usage: python3 gather_data_from_an_API.py <employee_id>')
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        sys.exit(1)
+
     get_employee_todo_progress(employee_id)
