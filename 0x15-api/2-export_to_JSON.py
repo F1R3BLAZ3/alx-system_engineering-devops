df59abb568dc75781e471ed60de96e0d47272ba7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-25 18:32:48 +0000

    Task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -21,6 +21,15 @@ def get_employee_todo_progress(employee_id):
     Returns:
         None
     """
+    # Fetch employee's TODO list
+    todo_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
+    todo_data = todo_response.json()
+
+    # Filter completed tasks
+    completed_tasks = [task for task in todo_data if task["completed"]]
+
     # Fetch employee name
     user_response = requests.get(
         f"https://jsonplaceholder.typicode.com/users/{employee_id}"
@@ -28,43 +37,24 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()
     employee_name = user_data.get("name")
 
-    # Fetch employee's TODO list
-    todo_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    )
-    todo_data = todo_response.json()
-
-    # Calculate completed and total tasks
-    completed_tasks = [task for task in todo_data if task["completed"]]
-    total_tasks = len(todo_data)
-    completed_task_cnt = len(completed_tasks)
-
-    # Print employee TODO list progress
-    print("Employee {} is done with tasks ({}/{}):".format(employee_name,
-                                                           completed_task_cnt,
-                                                           total_tasks))
-
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
-
     # Export data to JSON
-    export_to_json(employee_id, employee_name, completed_tasks)
+    export_to_json(employee_id, completed_tasks, employee_name)
 
 
-def export_to_json(employee_id, employee_name, completed_tasks):
+def export_to_json(employee_id, completed_tasks, employee_name):
     """
     Export employee's TODO list data to a JSON file.
 
     Args:
         employee_id (int): The ID of the employee.
-        employee_name (str): The name of the employee.
         completed_tasks (list): List of completed tasks.
+        employee_name (str): The name of the employee.
 
     Returns:
         None
     """
     data = {
-        "USER_ID": [
+        str(employee_id): [
             {
                 "task": task["title"],
                 "completed": task["completed"],
@@ -80,9 +70,9 @@ def export_to_json(employee_id, employee_name, completed_tasks):
         json.dump(data, json_file, indent=4)
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
+        print("Usage: python3 2-export_to_JSON.py <employee_id>")
         sys.exit(1)
 
     employee_id = sys.argv[1]
