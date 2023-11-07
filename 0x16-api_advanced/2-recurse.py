1d09ff4ca00478cb2a0f595e07c5f9dd423cdb54
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-07 13:46:45 +0000

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns a list containing
-the titles of all hot articles
-for a given subreddit. Uses a recursive approach for pagination.
-
-Requirements:
-- Uses the Requests module for sending HTTP requests to the Reddit API
-- Follows the PEP 8 style
+Queries the Reddit API recursively and returns a list
+of hot article titles for a given subreddit.
 """
 
 import requests
@@ -14,39 +9,38 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     """
-    Recursively fetches the titles of all hot articles for a subreddit.
+    Recursively fetches hot article titles from the Reddit API.
 
-    :param subreddit: The name of the subreddit.
-    :param hot_list: A list to store the titles (default is an empty list).
-    :param after: The 'after' parameter for pagination (default is None).
-    :return: A list of article titles or None if the subreddit is invalid.
+    Args:
+        subreddit (str): The subreddit to search for hot articles.
+        hot_list (list): A list to store hot article titles.
+        after (str): The "after" parameter used for pagination.
+
+    Returns:
+        list: A list containing hot article titles.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    if after:
-        url += f"?after={after}"
-
-    # Set a custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': 'MyRedditBot/1.0'}
+    params = {'limit': 100, 'after': after}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code == 200:
         try:
             data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                title = post['data']['title']
-                print(title)
+            children = data['data']['children']
+            for child in children:
+                hot_list.append(child['data']['title'])
 
             after = data['data']['after']
-            if after:
+            if after is not None:
                 return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
         except (KeyError, ValueError):
             # Invalid data or format
             return None
     else:
         # Invalid subreddit or other error
         return None
-
-    return hot_list
