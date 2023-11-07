b073bffa1d359f1c1036de216e57c2f6eff8c8ce
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-07 16:41:27 +0000

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 file { '/var/www/html/wp-settings.php':
   ensure => present,
-  owner  => 'www-data', # Replace with your Apache user
-  group  => 'www-data', # Replace with your Apache group
+  owner  => 'www-data',
+  group  => 'www-data',
   mode   => '0644',
-  source => 'puppet:///modules/module_name/wp-settings.php', # Replace with the actual file source
+  source => '/var/www/html/wp-settings.php',
 }
