2b60046342ae84259d9563966b5ff464bfea123b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-08 11:29:50 +0000

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# Puppet manifest to fix an issue with Wordpress not initializing
+
 exec { 'fix_phpp':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/bin', '/usr/bin', '/usr/local/bin'],
